1ded58cddb5f57d9d4b432509c36388f18f20d2a
Mehebul02 <mehebul2123@gmail.com>
2024-11-24 20:28:52 +0600

    product schema created successfully

---
+++ src/module/product/product.model.ts
@@ -0,0 +1,18 @@
+import { model, Schema } from "mongoose";
+
+
+const productSchema = new Schema({
+
+    name: { type: String, required: true },
+    brand: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, enum: ["Mountain", "Road", "Hybrid", "Electric"], required: true },
+    description: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    inStock: { type: Boolean, required: true },
+
+})
+
+const Product = model("Product",productSchema)
+
+export default Product
