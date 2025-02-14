fd4a0cb4ddf7ee6aa634f0172206765211a8bd86
Mehebul02 <mehebul2123@gmail.com>
2025-02-14 22:54:12 +0600

    Auth added the auth validation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { productRoutes } from "../module/product/product.router";
 import { orderRoutes } from "../module/order/order.router";
 import { userRoutes } from "../module/user/user.router";
+import { authRouter } from "../module/auth/auth.router";
 
 const router = Router();
 
@@ -17,6 +18,10 @@ const moduleRoutes = [
   {
     path:'/users',
     route:userRoutes
+  },
+  {
+    path:'/auths',
+    route:authRouter
   }
 
 ];
